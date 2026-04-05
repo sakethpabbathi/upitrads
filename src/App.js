10e1f7d9b94e0e20a4a4ab7f93f10775438100d7
sakethpabbathi <sakethpabbathi@gmail.com>
2026-04-05 11:54:39 +0530

    prepare for deployment

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
-    <BrowserRouter basename="/myseaexport">
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/imports-exports" element={<ImportsExportsPage />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter basename="/upitrads">
+    //     <BrowserRouter>
+    //   <ScrollToTop />
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />}/>
+    //     <Route path="/imports-exports" element={<ImportsExportsPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+    <BrowserRouter basename="/upitrads">
+  <ScrollToTop />
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/imports-exports" element={<ImportsExportsPage />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
